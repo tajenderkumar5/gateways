580ef8cb88d9f1e875094c12f1059c98ce74cfab
Tajender kumar <61531264+tajenderkumar5@users.noreply.github.com>
2021-12-11 11:31:11 +0530

    Update settings.py

--- razorpay/settings.py
+++ razorpay/settings.py
@@ -44,8 +44,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+   
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,7 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, 'staticfiles')
+   os.path.join(BASE_DIR, 'static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
