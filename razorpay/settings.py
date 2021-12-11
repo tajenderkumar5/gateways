f0351edc843d8106169fe6fcf496cce62e2dc230
Tajender kumar <61531264+tajenderkumar5@users.noreply.github.com>
2021-12-11 11:34:32 +0530

    Update settings.py

--- razorpay/settings.py
+++ razorpay/settings.py
@@ -127,7 +127,7 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
