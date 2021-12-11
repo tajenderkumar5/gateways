45755c130a8f959850a00f047eec7a6c8a34659f
Tajender kumar <61531264+tajenderkumar5@users.noreply.github.com>
2021-12-11 11:04:38 +0530

    Update settings.py

--- razorpay/settings.py
+++ razorpay/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
