63b2f2699a0544549b45f5d899b4ad114105069e
Tajender kumar <61531264+tajenderkumar5@users.noreply.github.com>
2021-12-11 11:08:03 +0530

    Update settings.py

--- razorpay/settings.py
+++ razorpay/settings.py
@@ -123,11 +123,10 @@ USE_TZ = True
 
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
 
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
